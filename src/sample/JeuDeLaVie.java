2eed869bfe32e50d6b9fe460aa6aa130991bde6f
Popo <pauline.lim0299@gmail.com>
2022-03-22 12:14:21 +0100

    rajout de tour.

--- src/sample/JeuDeLaVie.java
+++ src/sample/JeuDeLaVie.java
@@ -4,11 +4,11 @@ public class JeuDeLaVie {
 
     private int[][] tableau;
 
-    public JeuDeLaVie(int[][] tab){
+    public JeuDeLaVie(int[][] tab) {
         this.tableau = tab;
     }
 
-    public int[][] getTableau(){
+    public int[][] getTableau() {
         return this.tableau;
     }
 
@@ -16,37 +16,47 @@ public class JeuDeLaVie {
         this.tableau = tableau;
     }
 
-    public void setTableauIJ(int i, int j, int nb){
+    public void setTableauIJ(int i, int j, int nb) {
         this.tableau[i][j] = nb;
     }
 
-    public int valeurCase(int i, int j){
+    public int valeurCase(int i, int j) {
         return this.tableau[i][j];
     }
 
-    public int totalVoisins(int i, int j){
-        int somme =0;
-
-        for (int k = i-1; k < i+2; k++) {
-            for (int l = j-1; l <j+2 ; l++) {
-                if (k != i || l !=j){
-                    somme = somme + this.tableau[k][l];
+    public int totalVoisins(int i, int j) {
+        int somme= 0 ;
+        for (int ligne = i-1 ; ligne < i + 2; ligne++){
+            for (int colonne = j-1 ; colonne < j+2 ; colonne++){
+                if ((ligne != i || colonne != j)
+                        && (ligne != -1 && colonne!= -1)
+                        && (ligne != this.tableau.length && colonne != this.tableau[0].length)) {
+                    somme = somme + this.tableau[ligne][colonne];
                 }
             }
-
         }
         return somme;
     }
 
-    public int resultats(int valeur_case ,int valeur_voisins ){
+    public int resultats(int valeur_case, int valeur_voisins) {
         int resultat = valeur_case;
-        if(valeur_case==0 && valeur_voisins==3 ){ // la cellule est morte
+        if (valeur_case == 0 && valeur_voisins == 3) { // la cellule est morte
             resultat = 1;
         }
-        if(valeur_case==1 && (valeur_voisins<2 || valeur_voisins>3)){
+        if (valeur_case == 1 && (valeur_voisins < 2 || valeur_voisins > 3)) {
             resultat = 0;
         }
         return resultat;
     }
 
+    public void tour() {
+        int[][] t = new int[this.tableau.length][this.tableau[0].length];
+        for (int i = 0; i < this.tableau.length; i++) {
+            for (int j = 0; j < this.tableau[0].length; j++) {
+                t[i][j] = resultats(this.tableau[i][j], totalVoisins(i, j));
+            }
+
+        }
+        this.tableau = t;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,17 +13,37 @@ public class Main {
                 tab.setTableauIJ(i, j, 0);
             }
         }
-        tab.setTableauIJ(2, 1, 1);
-        tab.setTableauIJ(2, 3, 1);
-        tab.setTableauIJ(1, 2, 1);
+        tab.setTableauIJ(0, 1, 1);
+        tab.setTableauIJ(0, 0, 1);
+        tab.setTableauIJ(0, 2, 1);
+        //tab.setTableauIJ(2, 2, 1);
+
 
         //tab.setTableauIJ(1, 1, 1);
+        int valeur_voisins = tab.totalVoisins(2,2);
+        System.out.println(tab.resultats(tab.valeurCase(1,2), tab.totalVoisins(1,2)));
+        System.out.println("voisin 2, 2: " + (valeur_voisins < 2 || valeur_voisins > 3));
+        int valeur_voisins2 = tab.totalVoisins(2,1);
+        System.out.println("voisin 2, 1: " + (valeur_voisins2 < 2 || valeur_voisins2 > 3));
+        int valeur_voisins3 = tab.totalVoisins(2,1);
+        System.out.println("voisin 2, 3: " + tab.valeurCase(2,3));
 
+
+        System.out.println("\n");
+        System.out.println("\n");
+        for (int j = 0; j < tab.getTableau()[0].length; j++) {
+            System.out.println(Arrays.toString(tab.getTableau()[j]));
+        }
+        tab.tour();
+        System.out.println("\n");
+        System.out.println("\n");
         for (int j = 0; j < tab.getTableau()[0].length; j++) {
             System.out.println(Arrays.toString(tab.getTableau()[j]));
         }
 
-        System.out.println(tab.resultats(tab.valeurCase(2,2), tab.totalVoisins(2,2)));
+        //System.out.println(tab.resultats(tab.valeurCase(2,2), tab.totalVoisins(2,2)));
+
+
 
     }
 }
